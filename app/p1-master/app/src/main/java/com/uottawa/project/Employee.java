5d2d76c652d78f6d6ef3f4ef19781ad5eeb899bf
Angelica-P <apayn049@uottawa.ca>
2019-10-17 23:08:01 -0400

    Adding Account classes to Android Studio project.

---
+++ app/p1-master/app/src/main/java/com/uottawa/project/Employee.java
@@ -0,0 +1,31 @@
+package com.uottawa.project;
+
+public class Employee extends Account {
+
+    /*
+     * The clinic the employee belongs to.
+     */
+    private Clinic clinic;
+
+    /**
+     * Creates an Employee Account.
+     * @param password a string with the password
+     * @param username a string with the username
+     * @param firstName a string with the first name of the user
+     * @param lastName a string with the last name of the user
+     */
+    public Employee(String password, String username, String firstName, String lastName) {
+        super(password, username, firstName, lastName);
+    }
+
+
+    //This method will be implemented when the Employee functionallity is added.
+
+
+    /**
+     * Create a new Clinic.
+     */
+    public void createClinic(String name) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
