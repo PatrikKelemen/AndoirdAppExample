eef7f2896db299eeaa47c115d76640e8ee483054
PatrikKelemen <43449969+PatrikKelemen@users.noreply.github.com>
2019-11-03 21:38:45 -0500

    database fixes
    fixed up the database

--- app/p1-master/app/src/main/java/com/uottawa/project/DbHandler.java
+++ app/p1-master/app/src/main/java/com/uottawa/project/DbHandler.java
@@ -1,70 +1,56 @@
 package com.uottawa.project;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-//import android.database.sqlite.SQLiteOpenHelper;
-import java.io.File;
-import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Hashtable;
-//import android.database.DatabaseUtils;
+
+
+
+import java.util.List;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class DbHandler {
 
 
     // Initialize context
+    DatabaseReference database;
+    List<Account> users;
 
 
 
-    private SQLiteDatabase db;
     public DbHandler (){
-        String filePath = new File("").getAbsolutePath();
+        database = FirebaseDatabase.getInstance().getReference();
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange (DataSnapshot dataSnapshot){
+                users.clear();
 
-        db = SQLiteDatabase.openDatabase(filePath +"appDatabase.db", null, SQLiteDatabase.OPEN_READWRITE);
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Account user = postSnapshot.getValue(Account.class);
+                    users.add(user);
+                }
 
-    }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError){
 
+            }
+        });
 
-    public boolean dbSearch(String dataColumn, String accountType, String target){
-        return searchAllData(dataColumn, accountType).contains(target);
-    }
-
-    public void dbAdd (String username, String firstName, String lastName, String email, String password, String userType) {
-    }
-
-    public String matchData (String type, String name, String column ){
-        ArrayList<Account> userData;
-
-        int found = usernames.indexOf(name);
-        if (found == -1){
-            return "";
-        }
-        else{
-            return targetColumn.get(found);
         }
 
+
+
+    private void add(Account account) {
+
+
+        String id = database.push().getKey();
+
+
+
+        database.child(id).setValue(account);
     }
-
-    public ArrayList<String> searchAllData(String column, String profile) {
-        ArrayList<String> array_list = new ArrayList<String>();
-
-        Cursor res =  db.rawQuery( "select * from " + profile, null );
-        res.moveToFirst();
-
-        while(!res.isAfterLast()){
-            array_list.add(res.getString(res.getColumnIndex(column)));
-            res.moveToNext();
-        }
-        return array_list;
-    }
-
-
 }
