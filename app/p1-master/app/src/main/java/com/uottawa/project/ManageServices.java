e7126e0686310f3388f3ba03089f090aedd95f1c
Nolan-B <ndb17915@hotmail.ca>
2019-11-10 18:08:44 -0500

    Added create and edit services
    Will allow user to enter and edit services, does not write to database yet

--- app/p1-master/app/src/main/java/com/uottawa/project/ManageServices.java
+++ app/p1-master/app/src/main/java/com/uottawa/project/ManageServices.java
@@ -14,7 +14,7 @@ public class ManageServices extends AppCompatActivity {
 
     EditText editTextName;
     EditText editTextRate;
-    EditText editTextPerson;
+    EditText editTextRole;
     Button buttonAddService;
     ListView listViewServices;
     Button buttonExit;
@@ -27,12 +27,21 @@ public class ManageServices extends AppCompatActivity {
         setContentView(R.layout.activity_manage_services);
         editTextName = (EditText) findViewById(R.id.ServiceName);
         editTextRate = (EditText) findViewById(R.id.ServiceRate);
-        editTextPerson =(EditText) findViewById(R.id.ServiceJob);
+        editTextRole =(EditText) findViewById(R.id.ServiceJob);
         listViewServices = (ListView) findViewById(R.id.ServiceList);
         buttonAddService = (Button) findViewById(R.id.addService);
         buttonExit = (Button) findViewById(R.id.ExitService);
 
         services = new ArrayList<>();
+
+        listViewProducts.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Product service = services.get(i);
+                showUpdateDeleteDialog(service.getId(), service.getName(), service.getRate(), service.getRole());
+                return true;
+            }
+        });
 
     }
 
@@ -42,7 +51,10 @@ public class ManageServices extends AppCompatActivity {
 
     }
     public void onAdd(View view){
-
+        Service newService = new Service();
+        newService.setName(editTextName);
+        newService.setRate(editTextRate);
+        newService.setRole(editTextRole);
     }
 
 
